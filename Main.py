e630da1ef1ae2b9249808f2d9fa3326046891637
Juan <Juan29834@outlook.com>
2025-05-31 04:31:44 -0400

    Still in progress

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Main program to run simulations based ond 
 
 
@@ -19,18 +18,6 @@
 
 #player_list2_starters = [100,100,100,100,100]
 #player_list2_bench = [100,100,100,100,100]
-=======
-from teams.team import team
-from teams.player import player
-
-
-# Starters and Bench
-#player_list1_starters = [10, 10, 10, 10, 10]
-#player_list1_bench = [10, 10, 10, 10, 10]
-
-#player_list2_starters = [10, 10, 10,10,10]
-#player_list2_bench = [10,10,10,10,10]
->>>>>>> 67b6bd72760fb79cf7ee5d9cefae0474ce216083
 
 # Total power (starters + bench)
 #team1_total = sum(player_list1_starters) + sum(player_list1_bench)
@@ -39,20 +26,6 @@ from teams.player import player
 # Difference in power
 #power_diff = abs(team1_total - team2_total)
 
-<<<<<<< HEAD
-# Decision logic
-#if power_diff <= 7:
-    #print("Very close game\n Displaying win chances:")
-    #team1_win = team1_total / (team1_total + team2_total)#Divide the part by the whole
-    #team2_win = team2_total / (team1_total + team2_total)
-    #print(f"Team 1 win chance:{team1_win:.2%}")
-    #print(f"Team 1 win chance:{team2_win:.2%}")
-#elif team1_total > team2_total:
-    #print("Team 1 is favored to win.")
-#else:
-    #print("Team 2 is favored to win.")
-
-=======
 # Decision logic / will change
 #if power_diff <= 4:
   #  print("Very close game. Displaying win chances:")
@@ -64,7 +37,6 @@ from teams.player import player
  #print("Team 1 is favored to win.")
 #else:
  #   print("Team 2 is favored to win.")
->>>>>>> 67b6bd72760fb79cf7ee5d9cefae0474ce216083
 
 
     
--- teams/team.py
+++ teams/team.py
@@ -3,7 +3,11 @@ class Team:
     def __init__(self, team_id):
         self.team_id = team_id
         self.players = []
+    def team_list(self,players):
         for x in mock_data.mock_players:
-            if x["is_playing" and x"Team_id" == team_id]:
+            if(x["is_playing"] == True && Team_id == team_id ):
+                
+
+
         #Need to append to list and instantiate player class
 
