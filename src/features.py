65047e95ebce4857fa24609d0576b32f47285eeb
Juan Mendez <Juan29834@outlook.com>
2025-07-15 13:46:15 -0400

    Functions to be written and connectly closely with Main.py and preprocess.py

--- src/features.py
+++
@@ -1,7 +0,0 @@
-#use the clean data so we can use it to run calculations
-whole_yes_no = input("Would you like the entire logs for every nba player - Type [YES/NO] ") 
-whole_yes_no.lower()
-
-if whole_yes_no == 'YES':
-    import preprocess
-    preprocess.entire_logs()
