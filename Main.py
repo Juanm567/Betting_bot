351d81015da9ae6bafc1fbeb413881d5af393fbb
Juan Mendez <Juan29834@outlook.com>
2025-07-15 13:38:22 -0400

    Added handled logic to have user_input as arguments, that way v1 can be a CLI tool

--- Main.py
+++ Main.py
@@ -1,4 +1,36 @@
 #Handle class calls and the instantiation of the discord bot therefore this is the main entry point which will be what runs my entire data pipeline
+from src import features
+import sys
+#set up variable for the list of player names
+player_names = []
+instructions = {
+    'Player_games:' : " # of games to lookup, player name",
+    'Team_games:': " # of games to lookup, Team name",
+    'player_stats:': " player name, yes or no if you want a graph of points in recent games",
+    'Team_vs_Team:' : " Team1 name, Team2 name",
+    'Player_vs_Player:' : " Player1 name, Player2 name"
+    }
+command_actions = {
+   'player_games':features.player_games,#use dynamic arguments to decide argsfor functions in runtime
+   'team_games':features.team_games,
+   'player_stats':features.player_stats,
+   'team_vs_team':features.tvt,
+   'player_vs_player':features.pvp
+}
+print("\n\nWelcome to the easy NBA info lookup program so far we only support player and team data but machine learning predictions are in the works\n\n")
+
+print("What would you like to lookup?\n")
+print("Type 'done' to exit")
+for key , value in (instructions.items()):
+    print(f"{key}{instructions[key]}\n")
+while(True):
+ user_in= input("-->")
+ if user_in.lower() == 'done': sys.exit(0)
+ slicing = user_in.strip().split()
+ command = slicing[0]
+ args = slicing[1:]
+
+
 
 
 
